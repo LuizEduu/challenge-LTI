b1d00b14b0c85a7dfe54d288f9c7de7530dd93d7
Luiz <l.eduardo8316@gmail.com>
2024-08-03 17:26:15 -0300

    feat: create prisma benefits repository

---
+++ src/infra/database/prisma/mappers/prisma-benefit-mapper.ts
@@ -0,0 +1,26 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { Benefit } from '@/domain/RH/enterprise/entities/benefit';
+import { Prisma, Benefit as PrismaBenefit } from '@prisma/client';
+
+export class PrismaBenefitMapper {
+  static toDomain(raw: PrismaBenefit): Benefit {
+    return Benefit.create(
+      {
+        name: raw.name,
+        description: raw.description,
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+      },
+      new UniqueEntityID(raw.id),
+    );
+  }
+
+  static toPrisma(benefit: Benefit): Prisma.BenefitUncheckedCreateInput {
+    return {
+      id: benefit.id.toString(),
+      name: benefit.name,
+      description: benefit.description,
+      createdAt: benefit.createdAt,
+    };
+  }
+}
---
+++ src/infra/database/prisma/repositories/prisma-benefits-repository.ts
@@ -0,0 +1,30 @@
+import { BenefitsRepository } from '@/domain/RH/application/repositories/benefits-repository';
+import { Benefit } from '@/domain/RH/enterprise/entities/benefit';
+import { PrismaService } from '../prisma.service';
+import { PrismaBenefitMapper } from '../mappers/prisma-benefit-mapper';
+
+export class PrismaBenefitsRepository implements BenefitsRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async create(benefit: Benefit): Promise<void> {
+    const data = PrismaBenefitMapper.toPrisma(benefit);
+
+    await this.prisma.benefit.create({
+      data,
+    });
+  }
+
+  async findByName(name: string): Promise<Benefit | null> {
+    const benefit = await this.prisma.benefit.findFirst({
+      where: {
+        name,
+      },
+    });
+
+    if (!benefit) {
+      return null;
+    }
+
+    return PrismaBenefitMapper.toDomain(benefit);
+  }
+}

5db79a6ba20ec95f4d65bd58be33e3ffabd9943e
Luiz <l.eduardo8316@gmail.com>
2024-08-04 14:26:20 -0300

    feat: authenticate user use case

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { HttpModule } from './infra/http/http.module';
 import { DatabaseModule } from './infra/database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { CryptographyModule } from './infra/cryptography/cryptography.module';
+import { AuthModule } from './infra/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    AuthModule,
     HttpModule,
     DatabaseModule,
     CryptographyModule,

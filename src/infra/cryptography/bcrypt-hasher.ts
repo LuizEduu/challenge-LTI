4f3600015ddeed05a3101972563bbc5d2ec711da
Luiz <l.eduardo8316@gmail.com>
2024-08-04 14:28:05 -0300

    feat: implements comparer and encrypt interfaces

--- src/infra/cryptography/bcrypt-hasher.ts
+++ src/infra/cryptography/bcrypt-hasher.ts
@@ -1,12 +1,17 @@
+import { HashComparer } from '@/domain/RH/application/cryptography/hash-comparer';
 import { HashGenerator } from '@/domain/RH/application/cryptography/hash-generator';
 import { Injectable } from '@nestjs/common';
-import { hash } from 'bcryptjs';
+import { compare, hash } from 'bcryptjs';
 
 @Injectable()
-export class BcryptHasher implements HashGenerator {
+export class BcryptHasher implements HashGenerator, HashComparer {
   private HASH_SALT_LENGTH = 8;
 
   hash(plain: string): Promise<string> {
     return hash(plain, this.HASH_SALT_LENGTH);
+  }
+
+  async compare(plain: string, hash: string): Promise<boolean> {
+    return compare(plain, hash);
   }
 }
